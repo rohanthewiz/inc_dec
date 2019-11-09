a8806fa652a31e4bc3c8b6efd1454e2499f1895c
Rohan Allison <rohan.allison@cbre.com>
2019-11-08 20:29:35 -0600

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:inc_dec/views/home_page.dart';
+import 'package:provider/provider.dart';
+import 'package:inc_dec/providers/counter.dart';
+
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(builder: (_) => Counter(0)),
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+        ),
+        home: MyHomePage(title: 'Flutter Demo Home Page'),
+      ),
+    );
+  }
+}
+
